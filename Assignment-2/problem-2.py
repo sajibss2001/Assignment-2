dfea7ebf97cead8dba150571b48c2eb891bca5e2
Sajib Sannashi <sajibss2001@gmail.com>
2024-08-22 09:12:39 +0600

    Add files via upload

---
+++ Assignment-2/problem-2.py
@@ -0,0 +1,6 @@
+# Write a program that calculates the area of a rectangle given its length and width.
+
+width=int(input("Enter the rectangle width : "))
+length=int(input("Enter the rectangle length : "))
+area=width*length
+print(f"Rectangle Area : {area} unit") 
